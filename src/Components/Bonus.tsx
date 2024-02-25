076f681e1e55cbea5e2e62135007031a4686b7a4
Rohitash Sharma <rohitash.s@cubiclogics.com>
2024-02-25 18:43:41 +0530

    design

--- src/Components/Bonus.tsx
+++ src/Components/Bonus.tsx
@@ -12,6 +12,8 @@ const Bonus = () => {
         <div className="border border-white-500 p-4 mt-4">
             <h1 className="text-center font-bold text-orange-500 ">Bonus Componet </h1>
             <h2 className="text-yellow-500 font-bold text-center p-4">Total Point:{bonus.points}</h2>
+
+            
             <div className="flex justify-center gap-8">
                 <button className="font-semibold bg-gray-300 text-black p-1" onClick={IncrementPoints}>Inrement + </button>
                 {/* <button className="font-semibold bg-gray-300 text-black p-1">Decrement -</button>
